c468656538b85d5aa402e0a60ae7a9e4ef8535bb
Aaroh Mankad <aarohmankad@gmail.com>
2019-06-08 17:02:12 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	import SvelteDragula from './svelte-dragula.svelte';
+
+  const options = {
+    removeOnSpill: true
+  }
+</script>
+
+<style>
+</style>
+
+<SvelteDragula {options}>
+  <div class="dragula-slot">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati illo, impedit iste nesciunt quo explicabo tempore provident, fugiat nemo incidunt repellendus facere, voluptas eaque odit soluta unde voluptatum praesentium! Qui.</p>
+  </div>
+  <div class="dragula-slot">
+    <p>Quidem libero enim provident neque, ex omnis. Velit necessitatibus magni, voluptatum quis distinctio nesciunt vero fuga aliquid ipsum cupiditate neque excepturi, nemo sit non explicabo soluta autem, rem veniam ratione!</p>
+  </div>
+  <div class="dragula-slot">
+    <p>Ipsam eius, iusto laborum placeat aspernatur laudantium dolorem! Perspiciatis itaque aperiam, est eum commodi, doloremque fugit dicta consequatur! Molestias vel reprehenderit, officiis corrupti, sed nostrum quod expedita qui magni? Excepturi.</p>
+  </div>
+</SvelteDragula>
---
+++ src/svelte-dragula.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { onMount } from 'svelte';
+  import atoa from 'atoa';
+  import dragula from 'dragula';
+
+  import 'dragula/dist/dragula.min.css';
+
+  export let options = {};
+
+  onMount(async () => {
+    console.log(options)
+    const slots = document.querySelectorAll('.dragula-container > .dragula-slot');
+    const drake = dragula(atoa(slots), options);
+  })
+</script>
+<style>
+</style>
+
+<div class="dragula-container">
+  <slot></slot>
+</div>
